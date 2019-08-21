3c372bc1541b7de134c13d881ea107e379366e99
Sukhwan Yun <charintdouble@naver.com>
2019-08-21 07:57:52 +0000

    Initial

---
+++ RoutePlanningProject/src/route_model.cpp
@@ -0,0 +1,74 @@
+#include "route_model.h"
+#include <iostream>
+
+RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml)
+{
+    int index = 0;
+    for (Model::Node node : this->Nodes())
+    {
+        m_Nodes.push_back(RouteModel::Node(index, this, node));
+        index++;
+    }
+}
+
+void RouteModel::CreateNodeToRoadHashmap()
+{
+    for (const Model::Road &road : this->Roads())
+    {
+        if (road.type != Model::Road::Type::Footway)
+        {
+            for (int node_idx : Ways()[road.way].nodes)
+            {
+                if (node_to_road.find(node_idx) == node_to_road.end())
+                {
+                    node_to_road[node_idx] = std::vector<const Model::Road *>();
+                }
+                node_to_road[node_idx].push_back(&road);
+            }
+        }
+    }
+}
+
+RouteModel::Node *RouteModel::Node::FindNeighbor(std::vector<int> node_indices)
+{
+    Node *closest_node = nullptr;
+    for( int node_index : node_indices) {
+        RouteModel::Node node = this->parent_model->SNodes()[node_index];
+        if(!node.visited && this->distance(node) != 0) {
+            if(closest_node == nullptr || this->distance(node) < this->distance(*closest_node)) {
+                closest_node = &node;
+            }
+        }
+    }
+    return closest_node;
+}
+
+void RouteModel::Node::FindNeighbors() {
+    for( const Model::Road &road : parent_model->node_to_road[this->index]) {
+        RouteModel::Node *neighborNode = this->FindNeighbor(this->parent_model->Ways()[road.way].nodes);
+        if( neighborNode != nullptr){
+            this->neighbors.push_back(neighborNode);
+        }
+    }
+}
+
+RouteModel::Node &RouteModel::FindClosestNode(float x, float y) {
+    RouteModel::Node tempNode;
+    tempNode.x = x;
+    tempNode.y = y;
+    float min_dist = std::numeric_limits<float>::max();
+
+    int closest_idx;
+    for (const Model::Road &road : this->Roads()){
+        if(road.type != Model::Road::Type::Footway) {
+            for(int node_idx : Ways()[road.way].nodes) {
+                float dist = tempNode.distance(SNodes()[node_idx])
+                if(min_dist > dist) {
+                    min_dist = dist;
+                    closest_idx = node_idx;
+                }
+            }
+        }
+    }
+    return SNodes()[closest_idx];
+}
---
+++ RoutePlanningProject/src/route_planner.cpp
@@ -0,0 +1,71 @@
+#include "route_planner.h"
+#include <algorithm>
+
+RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+    start_x *= 0.01;
+    start_y *= 0.01;
+    end_x *= 0.01;
+    end_y *= 0.01;
+    this->start_node = &this->m_Model.FindClosestNode(start_x,start_y);
+    this->end_node = &this->m_Model.FindClosestNode(end_x,end_y);
+}
+
+vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node){ 
+    vector<RouteModel::Node> path_found;
+    this->distance = 0.0f;
+
+    RouteModel::Node parent;
+
+    while(current_node->parent != nullptr) {
+        path_found.push_back(*current_node);
+        parent = *(current_node->parent);
+        this->distance += parent.distance(*current_node);
+        current_node = &parent;
+    }
+    path_found.push_back(*current_node);
+    distance = m_Model.MetricScale();
+    return path_found;
+
+}
+
+void RoutePlanner::AStarSearch() {
+    start_node->visited = true;
+    open_list.push_back(start_node);
+    RouteModel::Node *current_node = nullptr;
+
+    while(open_list.size() > 0 ) {
+        current_node = NextNode();
+        if(current_node->distance(*end_node) == 0) {
+            m_Model.path = ConstructFinalPath(current_node);
+            return;
+        }
+        AddNeighbors(current_node);
+    }
+    
+}
+
+float RoutePlanner::CalculateHValue(RouteModel::Node *node) {
+    return node->distance(*end_node);
+}
+
+RouteModel::Node *RoutePlanner::NextNode() {
+    std::sort(open_list.begin(), open_list.end(), [](const RouteModel::Node *_1st, const RouteModel::Node *_2nd) {
+        return _1st->h_value + _1st->g_value < _2nd->h_value + _2nd->g_value;
+    });
+
+    RouteModel::Node *lowest_node = open_list.front();
+    open_list.erase(open_list.begin());
+    return lowest_node;
+}
+void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+    current_node->FindNeighbors();
+
+    for(RouteModel::Node *neighbor : current_node->neighbors) {
+        neighbor->parent = current_node;
+        neighbor->g_value = current_node->g_value + current_node->distance(*neighbor);
+        neighbor->h_value = CalculateHValue(neighbor);
+
+        open_list.push_back(neighbor);
+        neighbor->visited = true;
+    }
+}
