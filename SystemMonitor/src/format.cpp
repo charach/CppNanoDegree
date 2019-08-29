08edeb49e053c60317cb7db6e403029376e14c28
Sukhwan Yun <charintdouble@naver.com>
2019-08-29 23:25:06 +0900

    Add exception for uptime

--- SystemMonitor/src/format.cpp
+++ SystemMonitor/src/format.cpp
@@ -10,9 +10,15 @@ using std::string;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) {
-    int hour = seconds / 3600;
-    seconds -= hour * 3600;
-    int minutes = seconds / 60;
+    int hour,minutes;
+    hour = minutes = 0;
+    if(seconds > 3600){
+        hour = seconds / 3600;
+        seconds -= hour * 3600;
+    }
+    if(seconds > 60){
+        minutes = seconds / 60;
+    }
     seconds -= minutes * 60;
 
     return std::to_string(hour)+":"+std::to_string(minutes)+":"+std::to_string(seconds); 
